2644ecb5813d71edf1f361064f12d6e7d4c5317b
Sano <wkeifreire@gmail.com>
2021-09-10 21:23:20 -0400

    Update images.py

--- images.py
+++ images.py
@@ -5,7 +5,7 @@
 
 
 emotes = [
-    ("kms","https://cdn.discordapp.com/emojis/663077083337064459.png?v=1"),
+    ("kms-1","https://cdn.discordapp.com/emojis/663077083337064459.png?v=1"),
     ("bleh","https://cdn.discordapp.com/emojis/663076786359369738.png?v=1"),
     ("awoo","https://cdn.discordapp.com/emojis/831066730587619348.png?v=1"),
     ("genhum","https://cdn.discordapp.com/emojis/688112957460185207.png?v=1"),
@@ -19,18 +19,33 @@ emotes = [
     ("kannalick","https://cdn.discordapp.com/emojis/561630923892719626.png?v=1"),
     ("sakurathink","https://cdn.discordapp.com/emojis/635268683383701564.png?v=1"),
     ("comfigun","https://cdn.discordapp.com/emojis/566365703981236264.png?v=1"),
-    ("shrug","https://cdn.discordapp.com/emojis/663076977883873281.png?v=1"),
+    ("shrug-1","https://cdn.discordapp.com/emojis/663076977883873281.png?v=1"),
     ("ilyheart","https://cdn.discordapp.com/emojis/711733152208388117.png?v=1"),
     ("laugh-1","https://cdn.discordapp.com/emojis/716777304919113778.png?v=1"),
     ("smoosh","https://cdn.discordapp.com/emojis/706792596667629659.png?v=1"),
     ("woah-1","https://cdn.discordapp.com/emojis/798772861242048583.png?v=1"),
     ("blush-1","https://cdn.discordapp.com/emojis/746308523784405022.png?v=1"),
-    ("happy-1","https://cdn.discordapp.com/emojis/780943005557325824.gif?v=1"),
+    ("happy","https://cdn.discordapp.com/emojis/780943005557325824.gif?v=1"),
     ("drain","https://cdn.discordapp.com/emojis/737041622830940220.png?v=1"),
     ("wat","https://cdn.discordapp.com/emojis/813134380004147210.png?v=1"),
     ("blush-2","https://cdn.discordapp.com/emojis/751904754514526218.gif?v=1"),
     ("oki","https://cdn.discordapp.com/emojis/749005731147022367.gif?v=1"),
     ("smoosh-2","https://cdn.discordapp.com/emojis/746322920913109045.png?v=1"),
-    ("concern","https://cdn.discordapp.com/emojis/817387686565642251.gif?v=1")
+    ("concern","https://cdn.discordapp.com/emojis/817387686565642251.gif?v=1"),
+    ("baibai", "https://cdn.discordapp.com/emojis/812987549928521728.gif?v=1"),
+    ("asukasmug","https://cdn.discordapp.com/emojis/537095130428211213.png?v=1"),
+    ("blondcry","https://cdn.discordapp.com/attachments/813920772456054834/831020793836077076/826317986477506562.png"),
+    ("hug","https://cdn.discordapp.com/emojis/691093137573281842.png?v=1"),
+    ("sawnothing","https://cdn.discordapp.com/emojis/802890309066424340.png?v=1"),
+    ("hotsmug","https://cdn.discordapp.com/emojis/686290317582139493.png?v=1"),
+    ("peacecute-1","https://cdn.discordapp.com/attachments/813920772456054834/824050399517343785/529104339441025025.png"),
+    ("yatta","https://cdn.discordapp.com/emojis/529104156955508736.png?v=1"),
+    ("salutecute-1","https://cdn.discordapp.com/emojis/585995211725996042.png?v=1"),
+    ("salutepengi","https://cdn.discordapp.com/emojis/683496438629531686.png?v=1"),
+    ("ilyheart-1","https://cdn.discordapp.com/attachments/813920772456054834/813921003678990369/529467691543691284.png"),
+    ("ilyheart-2","https://cdn.discordapp.com/attachments/813920772456054834/813920856992382976/712142440953282600.png"),
+    ("02cry","https://cdn.discordapp.com/attachments/813920772456054834/877823634465488896/592066753182564373.png"),
+    ("blank","https://cdn.discordapp.com/emojis/599029376738263053.png?v=1"),
+    ("sigh-1","https://cdn.discordapp.com/emojis/781266153304621096.png?v=1")
 ]
 
